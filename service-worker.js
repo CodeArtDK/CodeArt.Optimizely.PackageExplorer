a3389bf89b57306ec5ca0b735a0ec973a828251c
AThraen <AThraen@users.noreply.github.com>
2025-11-14 20:42:16 +0000

    deploy: b10eaf8f715b9ae4927df69a3ce42f657ce12196

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: 7cSmEdVa */
+/* Manifest version: 3HU4Fp6s */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
